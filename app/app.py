919d3a5694d4b73178e60d99f28bcbebe5f641e6
Jayant Danech <jayant.danech@gmail.com>
2025-11-08 21:00:08 +0530

    update

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import os
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -7,7 +8,10 @@ app = Flask(__name__)
 def home():
     environment = os.getenv("APP_ENV", "development").capitalize()
     version = os.getenv("APP_VERSION", "v1.0")
-    return render_template('index.html', environment=environment, version=version)
+    date_time = os.get
+    formatted_date_time = current_datetime.strftime("%A, %B %d, %Y at %H:%M:%S")
+
+    return render_template('index.html', environment=environment, version=version, server_datetime=formatted_date_time)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
