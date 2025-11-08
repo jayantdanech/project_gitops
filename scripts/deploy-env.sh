328cd098a2cea18c6f537a089b3b3c0fb26c87a9
Jayant Danech <jayant.danech@gmail.com>
2025-11-08 20:38:47 +0530

    update

--- scripts/deploy-env.sh
+++ scripts/deploy-env.sh
@@ -10,13 +10,13 @@ env=${1:-dev}
 
 environ() {
     if [[ $env == "prod" ]]; then
-        port_info = "5004"
+        port_info="5004"
     elif [[ $env == "staging" ]]; then
-        port_info = "5003"
+        port_info="5003"
     elif [[ $env == "uat" ]]; then
-        port_info = "5002"
+        port_info="5002"
     elif [[ $env == "dev" ]]; then
-        port_info = "5001"
+        port_info="5001"
     fi
 
     echo -e "\n** DEPLOYMENT INFORMATION **\nProject Environment:  ${env}\nApp deployed! Visit http://localhost:${port_info}"
